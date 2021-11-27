ab7af05623ba4e683e57bdc7d058df9c31e97fbc
Sebastian <eljhoan1411@gmail.com>
2021-11-26 22:22:08 -0500

    metodos http get,post cliente

--- backend/routes/Cliente.py
+++ backend/routes/Cliente.py
@@ -1,9 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint,jsonify,request
+from sqlalchemy import select
+from sqlalchemy.orm import session
+from database.models.Empleado import Empleado
+from database.models.Cliente import Cliente
 from schemas.Cliente import ClienteSchema
+from database.db import Session
+
+session = Session
 bp = Blueprint('cliente',__name__)
 
 @bp.route('/clientes')
 
 def index_cliente():
-    
-    return "clientes"
+    schema = ClienteSchema(many=1)
+    stmt = select(Cliente)
+    result = session.execute(stmt).scalars().all()
+    clientes = schema.dump(result)
+    session.close()    
+    return jsonify(results = clientes)
+
+@bp.route('/cliente/<int:cliente_id>')
+
+def get_cliente(cliente_id):
+    schema = ClienteSchema()
+    stmt = select(Cliente).where(Cliente.id == cliente_id)
+    result = session.execute(stmt).scalars().one()
+    cliente = schema.dump(result)
+    return jsonify(cliente)
+
+@bp.route('/cliente',methods=['POST'])
+
+def post_cliente():
+    posted_cliente = ClienteSchema().load(request.get_json())
+    cliente = Cliente(**posted_cliente)
+    session.add(cliente)
+    session.commit()
+    new_cliente = ClienteSchema().dump(cliente)
+    session.close()
+    return jsonify(new_cliente),201
