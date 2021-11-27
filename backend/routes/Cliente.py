5a3d400c8bb70176799ad0ae14ebffb6bae4b6ed
Sebastian <eljhoan1411@gmail.com>
2021-11-26 21:09:41 -0500

    ruta para cliente

--- backend/routes/Cliente.py
+++ backend/routes/Cliente.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
-
+from schemas.Cliente import ClienteSchema
 bp = Blueprint('cliente',__name__)
 
 @bp.route('/clientes')
+
 def index_cliente():
+    
     return "clientes"
