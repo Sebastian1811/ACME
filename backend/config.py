f8bb11805f07eec9a59c22184c2b72ecdacfa336
Sebastian <eljhoan1411@gmail.com>
2021-11-27 01:01:09 -0500

    new config for bd

--- backend/config.py
+++ backend/config.py
@@ -6,5 +6,7 @@ load_dotenv()
 user = os.environ['DATABASE_USER']
 password = os.environ['DATABASE_PASSWORD']
 db = os.environ['DATABASE']
+host = os.environ['DATABASEHOST']
+port = os.environ['DATABASEPORT']
 
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@localhost/{db}'
+DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}'
