c9294ddeac290fb6bec7c9937e520317d15ee9aa
Sebastian <eljhoan1411@gmail.com>
2021-11-26 22:35:13 -0500

    fix

--- backend/routes/Mascota.py
+++ backend/routes/Mascota.py
@@ -1,7 +1,6 @@
 from flask import Blueprint,request,jsonify
 from sqlalchemy import select
 from sqlalchemy.orm import session
-from backend import schemas
 from database.db import Session
 from database.models.Mascota import Mascota
 from schemas.Mascota import MascotaSchema
