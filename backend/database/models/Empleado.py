ef145cff6f7b75917c4c97d825e5f4cbb972399d
Sebastian <eljhoan1411@gmail.com>
2021-11-26 21:44:38 -0500

    fix atributo empleado entity

--- backend/database/models/Empleado.py
+++ backend/database/models/Empleado.py
@@ -5,6 +5,6 @@ from sqlalchemy import Column,Integer
 class Empleado(Usuario,Base):
     __tablename__ = 'empleado'
     ventasTotales = Column(Integer)
-    def __init__(self,id,nombre,telefono,id_tipo,direccion,ventas):
-        super.__init__(id,nombre,telefono,id_tipo,direccion)
-        self.ventasTotales = ventas
+    def __init__(self,id,nombre,telefono,id_tipo,direccion,ventasTotales):
+        super().__init__(id,nombre,telefono,id_tipo,direccion)
+        self.ventasTotales = ventasTotales
