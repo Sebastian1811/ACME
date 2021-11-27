f9e8625aae8cc69d6bc1e9d7ef4aac31348e4ad2
Sebastian <eljhoan1411@gmail.com>
2021-11-26 20:17:33 -0500

    importe modulos

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,19 @@
 from flask import Flask
+from database.models.Usuario import Usuario
+from database.db import Base,Engine,Session
+from database.models.Empleado import Empleado
+from database.models.Cliente import Cliente
+from database.models.Mascota import Mascota
+from database.models.Producto import Producto
+from sqlalchemy import select
+from flask import Flask,jsonify,request
+from flask import Blueprint
 
 
+Base.metadata.create_all(Engine)
+session = Session
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hola,como estas"
