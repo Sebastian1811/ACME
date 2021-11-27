9e9e9edae30c1023c6b7f7a8b67ec84f28ac0975
Sebastian <eljhoan1411@gmail.com>
2021-11-26 22:21:54 -0500

    fix modelo cliente

--- backend/database/models/Cliente.py
+++ backend/database/models/Cliente.py
@@ -6,5 +6,5 @@ class Cliente(Usuario,Base):
     __tablename__ = 'cliente'
     email = Column(String)
     def __init__(self,id,nombre,telefono,id_tipo,direccion,email):
-        super.__init__(id,nombre,telefono,id_tipo,direccion,email)
+        super().__init__(id,nombre,telefono,id_tipo,direccion)
         self.email = email
