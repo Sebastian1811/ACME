0585ebf78beccfb5fd5be3ab1b53d70f35424c5b
Sebastian <eljhoan1411@gmail.com>
2021-12-09 00:03:10 -0500

    update schema

--- backend/schemas/Mascota.py
+++ backend/schemas/Mascota.py
@@ -1,6 +1,7 @@
 from marshmallow import Schema,fields
 
 class MascotaSchema(Schema):
+    id = fields.Number()
     id_propietario = fields.Number()
     nombre = fields.String()
     tipo = fields.String()
