b1c7eef68427ef21e18dad9c7a3883998a7cbeba
Sebastian <eljhoan1411@gmail.com>
2021-12-09 23:50:15 -0500

    add role column empleado

--- backend/database/models/Empleado.py
+++ backend/database/models/Empleado.py
@@ -6,7 +6,9 @@ class Empleado(Usuario,Base):
     __tablename__ = 'empleado'
     ventasTotales = Column(Integer,nullable=False)
     password = Column(String(256))
-    def __init__(self,id,nombre,apellido,telefono,id_tipo,direccion,ventasTotales,password):
+    role = Column(String())
+    def __init__(self,id,nombre,apellido,telefono,id_tipo,direccion,ventasTotales,password,role):
         super().__init__(id,nombre,apellido,telefono,id_tipo,direccion)
         self.ventasTotales = ventasTotales
         self.password = generate_password_hash(password)
+        self.role = role
