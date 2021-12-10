6fecbad38479f4253473332f80dc8cac6698e5b4
Sebastian <eljhoan1411@gmail.com>
2021-12-09 23:50:54 -0500

    imports factura y detalles

--- backend/routes/Factura.py
+++ backend/routes/Factura.py
@@ -1,16 +1,57 @@
-from flask import Blueprint,jsonify,request
+from flask import Blueprint,jsonify,request,abort
 from flask.blueprints import Blueprint
 from sqlalchemy import select
 from database.db import Session
-from database.models.Producto import Producto
+from database.models.Factura import Factura
+from database.models.Detalle import Detalle
 from schemas.Factura import FacturaSchema
 from schemas.Detalle import DetalleSchema
 
 bp = Blueprint('factura',__name__)
 session = Session
 
-@bp.route('/facturas')
+@bp.route('/facturar',methods=['POST'])
 
 def index_factura():
-    return "facturas"
+    solicitud = request.get_json()
+    detalles = solicitud['detalles']
+    try:
+        postedfactura = FacturaSchema().load(solicitud)
+        factura = Factura(**postedfactura)
+        session.add(factura)
+        session.commit()
+        session.close
+    except:
+        return abort(404)
 
+    try:
+        for i in detalles:
+            postedDetalle = DetalleSchema().load(i)
+            detalle = Detalle(**postedDetalle)
+            session.add(detalle)
+            session.commit()
+        session.close()
+    except:
+        return abort(404)        
+            
+
+    return "facturas"
+@bp.route('/facturas')
+
+def facturas():
+    schema = FacturaSchema(many=1)
+    stmt = select(Factura)
+    result = session.execute(stmt).scalars().all()
+    session.close()
+    facturas = schema.dump(result)
+    return jsonify(results = facturas)
+
+@bp.route('/detalles')
+
+def detalles():
+    schema = DetalleSchema(many=1)
+    stmt = select(Detalle)
+    result = session.execute(stmt).scalars().all()
+    session.close()
+    detalles = schema.dump(result)
+    return jsonify(results = detalles)    
