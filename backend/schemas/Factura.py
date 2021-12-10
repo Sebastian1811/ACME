30c1fd47d3ee8793514566fa26bd902771aea367
Sebastian <eljhoan1411@gmail.com>
2021-12-09 23:53:37 -0500

    exclude schema factura

--- backend/schemas/Factura.py
+++ backend/schemas/Factura.py
@@ -1,6 +1,8 @@
-from marshmallow import Schema,fields
+from marshmallow import Schema,fields,EXCLUDE
 
 class FacturaSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
     id_factura = fields.Number()
     id_cliente = fields.Number()
     fecha = fields.DateTime()
