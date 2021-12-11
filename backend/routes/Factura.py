5637d83005c075586e4218bee247db971d233c7c
Sebastian <eljhoan1411@gmail.com>
2021-12-10 23:17:06 -0500

    get details factura

--- backend/routes/Factura.py
+++ backend/routes/Factura.py
@@ -1,11 +1,13 @@
 from flask import Blueprint,jsonify,request,abort
-from flask.blueprints import Blueprint
 from sqlalchemy import select
+from database.models.Producto import Producto
 from database.db import Session
 from database.models.Factura import Factura
 from database.models.Detalle import Detalle
 from schemas.Factura import FacturaSchema
 from schemas.Detalle import DetalleSchema
+from schemas.Producto import ProductoSchema
+from sqlalchemy.sql.expression import join
 
 bp = Blueprint('factura',__name__)
 session = Session
@@ -23,7 +25,6 @@ def index_factura():
         session.close
     except:
         return abort(404)
-
     try:
         for i in detalles:
             postedDetalle = DetalleSchema().load(i)
@@ -35,7 +36,7 @@ def index_factura():
         return abort(404)        
             
     return "facturas"
-    
+
 @bp.route('/facturas')
 
 def facturas():
@@ -46,12 +47,48 @@ def facturas():
     facturas = schema.dump(result)
     return jsonify(results = facturas)
 
-@bp.route('/detalles')
+@bp.route('/factura/<int:id_factura>')
 
-def detalles():
-    schema = DetalleSchema(many=1)
-    stmt = select(Detalle)
-    result = session.execute(stmt).scalars().all()
+def get_factura(id_factura):
+    schema = FacturaSchema()
+    stmt = select(Factura).where(id_factura)
+    result = session.execute(stmt).scalars().one()
     session.close()
-    detalles = schema.dump(result)
-    return jsonify(results = detalles)    
+    factura = schema.dump(result)
+    return jsonify(factura)    
+
+@bp.route('/factura/details/<int:factura_id>')
+
+def factura(factura_id):
+    schemaFactura = FacturaSchema()
+    schemaDetalle = DetalleSchema(many=1)
+    schemaProducto = ProductoSchema(many=1)
+
+    Join = join(Factura,Detalle,Factura.id_factura == Detalle.id_factura)
+    Join2 = join(Detalle,Producto,Detalle.id_producto == Producto.id)
+
+    stmt = select(Factura).where(Factura.id_factura==factura_id)
+    stmt2 = select(Detalle).select_from(Join)
+    stmt3 = select(Producto).select_from(Join2).where(Detalle.id_factura == factura_id)
+    
+    try:
+        result = session.execute(stmt).scalars().one()
+        facturas = schemaFactura.dump(result)
+    except:
+        return abort(404)    
+    try:
+        result = session.execute(stmt2).scalars().all()
+        detalles = schemaDetalle.dump(result)
+    except:
+        return abort(404)
+    try:
+
+        result = session.execute(stmt3).scalars().all()
+        productos = schemaProducto.dump(result)
+    except:
+        return abort(404)       
+    session.close()
+    return jsonify(facturas = facturas,detalles = detalles,productos =productos)
+
+
+    
