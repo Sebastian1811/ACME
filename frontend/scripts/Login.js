7f7aa3484fdb7f2bbfa320f8724a10aad344e9d9
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2021-12-13 02:13:21 -0500

    Se quita la parte de script de Facturacion e Inventario. Se cambia Inventario por el nuevo. Se agregan comentarios al js del login.

---
+++ frontend/scripts/Login.js
@@ -0,0 +1,23 @@
+// Para sacar la URL de la API, la variable se llama API_URL, está en funcExternas.js, es para no escribir la URL por todos lados.
+// <script src="../scripts/funcExternas.js"></script>  <-- Eso es para que cargue la variable global y otras funciones por si las necesita, 
+//			aunque no creo que sea necesario.
+
+/*
+var url = 'https://example.com/profile';
+var data = {username: 'example'};
+
+fetch(url, {
+  method: 'POST', // or 'PUT'
+  body: JSON.stringify(data), // data can be `string` or {object}!
+  headers:{
+    'Content-Type': 'application/json'
+  }
+}).then(res => res.json())
+.catch(error => console.error('Error:', error))
+.then(response => console.log('Success:', response));
+*/
+
+// Algo así es para enviar datos, el data lo hace concatenando lo que halla en usuario y contraseña y ya.
+// La URL, lo que dije, solo es escribir API_URL.
+
+
