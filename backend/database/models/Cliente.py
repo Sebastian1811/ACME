2e58b533e2e5e6aea57047bf4ec1df69629e052d
Sebastian <eljhoan1411@gmail.com>
2021-12-13 21:15:00 -0500

    add ciudad column

--- backend/database/models/Cliente.py
+++ backend/database/models/Cliente.py
@@ -5,6 +5,8 @@ from sqlalchemy import Column,String
 class Cliente(Usuario,Base):
     __tablename__ = 'cliente'
     email = Column(String,nullable=False)
-    def __init__(self,id,nombre,apellido,telefono,id_tipo,direccion,email):
+    ciudad = Column(String)
+    def __init__(self,id,nombre,apellido,telefono,id_tipo,direccion,email,ciudad):
         super().__init__(id,nombre,apellido,telefono,id_tipo,direccion)
         self.email = email
+        self.ciudad = ciudad
