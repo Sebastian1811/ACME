8eb6d529c22ab616b9b79c4f8aa297fa24ac0ab4
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2021-12-13 23:07:17 -0500

    Se agregan nuevos archivos para nuevo cliente y nueva mascota

---
+++ frontend/scripts/nuevaMascota.js
@@ -0,0 +1,11 @@
+function mostrarDatos(){
+  var name = document.getElementById("nombres").value;
+  var surname = document.getElementById("apellidos").value;
+  var nombreCompleto = name + " " + surname;
+  var id = document.getElementById("cedula").value;
+  var x = document.getElementById("cargo").selectedIndex;
+  var y = document.getElementById("cargo").options;
+  document.getElementById("modalNombres").innerHTML = nombreCompleto;
+  document.getElementById("modalCedula").innerHTML = id;
+  document.getElementById("modalCargo").innerHTML = y[x].text;
+}
