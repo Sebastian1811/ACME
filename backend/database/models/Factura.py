c9514863cf887aae555fcad6d60ed98098404fe6
Sebastian <eljhoan1411@gmail.com>
2021-12-13 21:15:18 -0500

    add id empleado column to factura

--- backend/database/models/Factura.py
+++ backend/database/models/Factura.py
@@ -4,12 +4,13 @@ from ..db import Base
 
 class Factura(Base):
     __tablename__ = 'factura'
-    id_factura = Column(Integer,primary_key=True,autoincrement=False)
+    id_factura = Column(Integer,primary_key=True,autoincrement=1)
     id_cliente = Column(Integer,ForeignKey('cliente.id'))
+    id_empleado = Column(Integer,ForeignKey('empleado.id'))
     fecha = Column(DateTime,nullable=False)
 
-    def __init__(self,id_factura,id_cliente,fecha):
-        self.id_factura = id_factura
+    def __init__(self,id_cliente,id_empleado,fecha):
         self.id_cliente = id_cliente
+        self.id_empleado = id_empleado
         self.fecha = fecha
 
