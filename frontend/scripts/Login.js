7b73248c8ca95df22074318923437c7932a941c2
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2021-12-15 00:43:35 -0500

    Login funcionando (Parcialmente) y cambio pequeño a hub

--- frontend/scripts/Login.js
+++ frontend/scripts/Login.js
@@ -1,21 +1,14 @@
-// Para sacar la URL de la API, la variable se llama API_URL, está en funcExternas.js, es para no escribir la URL por todos lados.
-// <script src="../scripts/funcExternas.js"></script>  <-- Eso es para que cargue la variable global y otras funciones por si las necesita, 
-//			aunque no creo que sea necesario.
-
-var formulario = document.getElementById('formulario');
+let formulario = document.getElementById('formulario');
 
 formulario.addEventListener('submit', function(e){
   e.preventDefault();
-  console.log('click')
   
   var datos = new FormData(formulario);
 
-  console.log(datos);
   document.getElementById('Cedula').value = "";
   document.getElementById('Contra').value = "";
   var data = {username: parseInt(datos.get('Cedula')),password: datos.get('pass')};
 
-  console.log(data);
   fetch(API_URL+"/login", {
     method: 'POST', // or 'PUT'
     body: JSON.stringify(data), // data can be `string` or {object}!
@@ -24,17 +17,14 @@ formulario.addEventListener('submit', function(e){
     }
 
   }).then(res => res.json())
-  .catch(error => console.error('Error:', error))
   .then(response => {
-    console.log('Success:', response)
-    window.location.href = "hub.html";
-    location.reload();
-  });
-
+    let url = window.location.href;
+    url += "hub.html";
+    window.location = url;
+  })
+  .catch(err => {
+    var myModal = new bootstrap.Modal(document.getElementById("datosIncorrectos"));
+    document.getElementById("errorDatosIncorrectos").innerHTML = err;
+    myModal.show();
+  })
 })
-
-
-// Algo así es para enviar datos, el data lo hace concatenando lo que halla en usuario y contraseña y ya.
-// La URL, lo que dije, solo es escribir API_URL.
-
-
