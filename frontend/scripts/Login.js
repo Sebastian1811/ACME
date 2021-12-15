1e3a9ba81f6a8baee88b1f2e4c6e59df46a30e1a
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2021-12-15 18:15:46 -0500

    Limpieza y mejora para el login

--- frontend/scripts/Login.js
+++ frontend/scripts/Login.js
@@ -3,11 +3,9 @@ let formulario = document.getElementById('formulario');
 formulario.addEventListener('submit', function(e){
   e.preventDefault();
   
-  var datos = new FormData(formulario);
+  let datos = new FormData(formulario);
 
-  document.getElementById('Cedula').value = "";
-  document.getElementById('Contra').value = "";
-  var data = {username: parseInt(datos.get('Cedula')),password: datos.get('pass')};
+  let data = {username: parseInt(datos.get('Cedula')),password: datos.get('pass')};
 
   fetch(API_URL+"/login", {
     method: 'POST', // or 'PUT'
@@ -18,12 +16,14 @@ formulario.addEventListener('submit', function(e){
 
   }).then(res => res.json())
   .then(response => {
+    document.getElementById('Cedula').value = "";
+    document.getElementById('Contra').value = "";
     let url = window.location.href;
     url += "hub.html";
     window.location = url;
   })
   .catch(err => {
-    var myModal = new bootstrap.Modal(document.getElementById("datosIncorrectos"));
+    let myModal = new bootstrap.Modal(document.getElementById("datosIncorrectos"));
     document.getElementById("errorDatosIncorrectos").innerHTML = err;
     myModal.show();
   })
