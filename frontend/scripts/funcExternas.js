b265479537ca8fd291cc7e40016e528f3f90b576
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2021-12-16 01:37:31 -0500

    Se agrega función global para sortear ascendentemente JSONs a partir de un atributo

--- frontend/scripts/funcExternas.js
+++ frontend/scripts/funcExternas.js
@@ -61,3 +61,15 @@ function limpiarNumero(string){
   valor = parseInt(valor);
   return valor
 }
+
+// SORTEA UN JSON EN FORMA ASCENDENTE DE UN ATRIBUTO
+function GetSortOrder(prop) {    
+    return function(a, b) {    
+        if (a[prop] > b[prop]) {    
+            return 1;    
+        } else if (a[prop] < b[prop]) {    
+            return -1;    
+        }    
+        return 0;    
+    }    
+} 
