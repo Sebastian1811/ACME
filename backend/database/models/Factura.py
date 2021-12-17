8d7b09567d10e8586fead481b3c6c9a749226fa1
Sebastian <eljhoan1411@gmail.com>
2021-12-17 14:49:45 -0500

    remove Fkeys from factura

--- backend/database/models/Factura.py
+++ backend/database/models/Factura.py
@@ -5,8 +5,8 @@ from ..db import Base
 class Factura(Base):
     __tablename__ = 'factura'
     id_factura = Column(Integer,primary_key=True,autoincrement=0)
-    id_cliente = Column(Integer,ForeignKey('cliente.id'))
-    id_empleado = Column(Integer,ForeignKey('empleado.id'))
+    id_cliente = Column(Integer)#,ForeignKey('cliente.id'))
+    id_empleado = Column(Integer)#,ForeignKey('empleado.id'))
     fecha = Column(DateTime,nullable=False)
 
     def __init__(self,id_factura,id_cliente,id_empleado,fecha):
