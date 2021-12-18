9ec7ee93899a72a4f5ab4140a30aed59959917d6
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2021-12-17 20:36:23 -0500

    Borrado carpeta "backend" en la rama de "frontend"

--- backend/database/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine 
-from sqlalchemy.orm import Session 
-from sqlalchemy.ext.declarative import declarative_base
-from config import DATABASE_CONNECTION_URI
-
-Engine = create_engine(DATABASE_CONNECTION_URI)
-Base = declarative_base()
-Session = Session(Engine,future=True)
